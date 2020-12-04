7a95c95d9d4693d980defde2b011585c402f745a
Dani Baas <Dani.raj25@gmail.com>
2020-12-04 15:08:39 +0100

    Added Images Created Login screen

--- app/src/main/java/nl/danibaas/dailyhelper/MainActivity.java
+++ app/src/main/java/nl/danibaas/dailyhelper/MainActivity.java
@@ -1,13 +1,32 @@
 package nl.danibaas.dailyhelper;
 
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean hqMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeHQMode(boolean newValue) {
+        hqMode = newValue;
+        if (newValue) {
+            // TODO: set hq mode pictures
+            ImageView imv = findViewById(R.id.background);
+            imv.setImageResource(R.drawable.hq1);
+        } else {
+            // TODO: set safe mode pictures
+        }
+    }
+
+    public void LoginButtonClick(View view) {
+        changeHQMode(!hqMode);
     }
 }
