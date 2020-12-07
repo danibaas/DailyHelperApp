836a30873ea469dc0d219f6e6c88e0a95871a6ce
Dani Baas <Dani.raj25@gmail.com>
2020-12-07 15:31:46 +0100

    Replaced joker img. Created new page and strings.

--- app/src/main/java/nl/danibaas/dailyhelper/PasswordChecker.java
+++ app/src/main/java/nl/danibaas/dailyhelper/PasswordChecker.java
@@ -1,15 +1,12 @@
 package nl.danibaas.dailyhelper;
 
-import android.view.View;
-import android.webkit.ConsoleMessage;
 import android.widget.EditText;
 
 public class PasswordChecker {
 
-    private final String pw = "IMadeThisMyself#2020";
-
     public void checkPassword() {
         EditText passwordField = MainActivity.getInstance().findViewById(R.id.PasswordEntry);
+        final String pw = "IMadeThisMyself#2020";
         if (passwordField.getText().toString().equals(pw)) {
             // TODO: Direct onto main screen (user is now logged in)
             System.out.println("logged in");
