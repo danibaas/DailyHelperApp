86fee45e83b1a9faa58854e69081ee3ed60fca42
Dani Baas <Dani.raj25@gmail.com>
2020-12-07 01:10:57 +0100

    Restarted project due to Kotlin Update. First screen now leads to login screen.

--- app/src/main/java/nl/danibaas/dailyhelper/MainActivity.java
+++ app/src/main/java/nl/danibaas/dailyhelper/MainActivity.java
@@ -1,13 +1,45 @@
 package nl.danibaas.dailyhelper;
 
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean hqMode;
+    public PasswordChecker pw;
+
+    private static MainActivity instance;
+
+    public static MainActivity getInstance() {
+        return instance;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance = this;
+        pw = new PasswordChecker();
         setContentView(R.layout.activity_main);
+    }
+
+    private void changeHQMode(boolean newValue) { // change mode and all pictures
+        hqMode = newValue;
+        if (newValue) {
+            // TODO: set hq mode pictures
+            ImageView imv = findViewById(R.id.MainBackground);
+            //imv.setImageResource(R.drawable.hq1);
+        } else {
+            // TODO: set safe mode pictures
+        }
+    }
+
+    public void LoginButtonClick(View view) {
+        setContentView(R.layout.login_screen);
+    }
+
+    public void LoginButton(View view) {
+        pw.checkPassword();
     }
 }
